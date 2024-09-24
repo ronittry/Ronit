3e5edd332d7c9dae2f9b0cfb320e26b70bdffc05
Ronitanand0000 <113673966+Ronitanand0000@users.noreply.github.com>
2024-09-24 21:42:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY Ghost
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7451198481:AAFnvkRe1ktiAihVsTcvDEm2EUp8qNlNkCI")
+BOT_TOKEN=("7412698601:AAGPnsp16TWWqFAThg4s7zOEJe6OK2Ko3P8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("Samahi18_09")
+OWNER_USERNAME=("RONIT_IN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5870168842")
+ADMIN_IDS=("7246521618")
 
 
 
